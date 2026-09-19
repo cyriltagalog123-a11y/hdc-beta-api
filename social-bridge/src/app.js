c943dec8dccc7d9533e0e78f061e1f3ed64f9b97
cyriltagalog123-a11y <cyril.tagalog123@gmail.com>
2026-09-19 09:35:50 +0800

    Social Bridge: temporarily open initial Facebook OAuth setup route

--- social-bridge/src/app.js
+++ social-bridge/src/app.js
@@ -69,7 +69,9 @@ export async function createApp() {
     }));
   });
 
-  app.get('/auth/facebook', basicAdmin, (_req, res) => {
+  // Temporary setup route: left unauthenticated only for the initial Meta OAuth connection.
+  // After the Page connection succeeds, restore basicAdmin on this route.
+  app.get('/auth/facebook', (_req, res) => {
     res.redirect(buildFacebookLoginUrl(createOAuthState()));
   });
 
